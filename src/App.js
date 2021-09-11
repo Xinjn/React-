0ff1bb82a995a6477c5a27d96914b58e41bf46e0
Xinjn <422075616@qq.com>
2021-09-11 23:56:54 +0800

    实现增加功能(setState设置数据/ID自增/父子数据传递/input回车事件)

--- src/App.js
+++ src/App.js
@@ -3,24 +3,52 @@ import './App.css';
 import TodoInput from './components/TodoInput';
 import TodoItem from './components/TodoItem';
 
+let id = 0
+function idMaker() {
+  id += 1
+  return id
+}
+
 class App extends React.Component{
   constructor(props) {
     super(props)
     this.state = {
-      newTodo: 'test',
+      newTodo: '',
       todoList: [
-        { id: 1, title: '吃饭', type: '' },
-        { id: 2, title: '睡觉', type: '' },
-        { id: 3, title: '学前端', type: '' }
+        { id: idMaker(), title: '吃饭', type: '' },
+        { id: idMaker(), title: '睡觉', type: '' },
+        { id: idMaker(), title: '学前端', type: '' }
       ]
     }
+  }
+  addItem = () => {
+    const newItem = { id:idMaker(), title:this.state.newTodo, type: '' }
+    this.setState(state =>
+      ({
+        todoList:state.todoList.concat(newItem)
+      })
+    )
+  }
+  changeTitle = (e) => { 
+    this.setState(state =>
+      ({
+        newTodo:state.newTodo = e.target.value
+      })
+    )
   }
   render() {
     return (
       <div className="App">
         <h1>我的待办</h1>
-        <TodoInput value={this.state.newTodo} />
-        <button>新增</button>
+
+        <TodoInput
+          value={this.state.newTodo}
+          onChange = {this.changeTitle} //声明回调函数，传给子组件，传入表单数据
+          onSubmit={this.addItem}
+        />
+        
+        <button onClick={this.addItem} >新增</button>
+
         <TodoItem value={this.state.todoList}/>
       </div>
     )
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -3,12 +3,24 @@ import React from 'react';
 class TodoInput extends React.Component{
   constructor(props) {
     super(props)
-
+  }
+  sumbit = (e) => {
+    if (e.keyCode === 13) {
+      console.log('提交')
+      this.props.onSubmit.call()
+    }
+  }
+  changeTitle = (e) => {
+    this.props.onChange(e) //获取父组件会回调函数，传入表单数据
   }
   render() {
     return (
       <>
-        <input defaultValue = {this.props.value}/>
+        <input
+          defaultValue={this.props.value}
+          onChange = {this.changeTitle}
+          onKeyDown={this.sumbit}
+        />
       </>
     )
   }
