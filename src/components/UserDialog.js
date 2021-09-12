19fb01dd1003f8795a10d85f6f11bc015299fc4c
Xinjn <422075616@qq.com>
2021-09-12 11:25:36 +0800

    添加 UserDialog.css

--- src/components/UserDialog.js
+++ src/components/UserDialog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import "../css/UserDialog.css"
 
 class UserDialog extends React.Component{
     constructor(props) {
@@ -12,32 +13,32 @@ class UserDialog extends React.Component{
                         <input type="radio"/>注册
                         <input type="radio"/>登录
                     </nav>
-                    <div>
+                    <div className="panes">
                         {/* 注册 */}
                         <form className="sign">
-                            <div>
+                            <div className="row">
                                 <label>用户名</label>
                                 <input type="text"/>
                             </div>
-                            <div>
+                            <div className="row">
                                 <label>密码</label>
-                                <input type="text"/>
+                                <input type="password"/>
                             </div>
-                            <div>
+                            <div className="row actions">
                                 <button>注册</button>
                             </div>
                         </form>
                         {/* 登录 */}
                         <form className="login">
-                            <div>
+                            <div className="row">
                                 <label>用户名</label>
                                 <input type="text"/>
                             </div>
-                            <div>
+                            <div className="row">
                                 <label>密码</label>
-                                <input type="text"/>
+                                <input type="password"/>
                             </div>
-                            <div>
+                            <div className="row actions">
                                 <button>登录</button>
                             </div>
                         </form>
